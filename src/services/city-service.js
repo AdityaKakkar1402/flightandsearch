c10b011e2234b5596e3e793d563976eca9bf58d7
ADITYA KAKKAR <adi@ADITYAs-MacBook-Air.local>
2023-04-05 10:34:37 +0530

    setup service layer

---
+++ src/services/city-service.js
@@ -0,0 +1,44 @@
+const { CityRepository } = require("../repositories/index");
+
+class CityRepository {
+  constructor() {
+    this.cityRepository = new CityRepository();
+  }
+
+  async createCity(data) {
+    try {
+      const city = await this.cityRepository.createCity(data);
+      return city;
+    } catch (err) {
+      console.log("something went wrong in service layer");
+      throw { error };
+    }
+  }
+  async deleteCity(cityId) {
+    try {
+      const response = await this.cityRepository.deleteCity(cityId);
+      return response;
+    } catch (err) {
+      console.log("something went wrong in service layer");
+      throw { error };
+    }
+  }
+  async updateCity() {
+    try {
+      const city = await this.cityRepository.updateCity(cityId, data);
+      return city;
+    } catch (err) {
+      console.log("something went wrong in service layer");
+      throw { error };
+    }
+  }
+  async getCity() {
+    const city = await this.cityRepository.getCity(cityId);
+    return city;
+    try {
+    } catch (err) {
+      console.log("something went wrong in service layer");
+      throw { error };
+    }
+  }
+}
