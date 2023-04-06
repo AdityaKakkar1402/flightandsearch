60212eda1e02fd71fb4de566cb339d7de8055bfc
ADITYA KAKKAR <adi@ADITYAs-MacBook-Air.local>
2023-04-06 09:25:15 +0530

    added filter for flights

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -21,4 +21,24 @@ const create = async (req, res) => {
     });
   }
 };
-module.exports = { create };
+const getAll = async (req, res) => {
+  try {
+    const response = await flightService.getAllFlightData(req.query);
+    return res.status(201).json({
+      data: response,
+
+      success: true,
+      err: {},
+      message: "Successfully fetch the flights",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to fetch a flight",
+      err: error,
+    });
+  }
+};
+module.exports = { create, getAll };
